ab847dc064895e0e064dd7866c6e892b659fb54c
coderjazz18 <coderjazz@gmail.com>
2022-10-05 15:13:23 -0700

    Player 1, Welcome, and Categories established.

---
+++ CS101_KnockKnock.py
@@ -0,0 +1,10 @@
+
+player = input("\nWelcome to the World of Knock Knock Jokes! Now please tell us, who is there?\n")
+joke_categories = ['Animal', 'Food', 'Scary', 'Out of this WORLD!']
+print(f"\nNow, {player}, from the categories shown, please choose your category:")
+for joke in joke_categories:
+    print(joke)
+
+player_category = input("\nChoose wisely.... ")
+
+print("\nExcellent Choice!")
