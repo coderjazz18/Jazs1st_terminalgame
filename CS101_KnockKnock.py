ef4fc034292ec68a0d3d19b5cdf3290c4a538ffb
coderjazz18 <coderjazz@gmail.com>
2022-10-05 15:35:21 -0700

    Joke Steps added to KnockKnockJokes CLass

--- CS101_KnockKnock.py
+++ CS101_KnockKnock.py
@@ -10,6 +10,18 @@ class KnockKnockJokes:
     def __repr__(self):
         message = f"So you've chosen to do the most bamboozlin of {self.category} Knock Knock Jokes! Very well, we shall begin!"
         return message
+    
+    def InitalKnock(self):
+        print("Knock Knock...")
+
+    def whosThere(self, response, punchline_pt1):
+        if response == str and (response == "Whos there?" or "whos there" or 'whos there?' or 'WHOS THERE?'):
+            return punchline_pt1
+    
+    def punchline(self, who_response, punchline_pt1, final_punchline):
+        if who_response == f"{punchline_pt1} who?":
+            return final_punchline + "Ba Dum Ta Tuss!"
+
 
 
 player = input("\nWelcome to the World of Knock Knock Jokes! Now please tell us, who is there?\n")
