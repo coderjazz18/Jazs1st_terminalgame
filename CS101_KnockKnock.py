479e61b758950ed4c18013a42c07353d055ff27c
coderjazz18 <coderjazz@gmail.com>
2022-10-23 11:19:35 -0700

    Back for Review

--- CS101_KnockKnock.py
+++ CS101_KnockKnock.py
@@ -1,8 +1,6 @@
 # File finally acting as it should
 import random
-from xmlrpc.client import ResponseError
 # this module will allow a random joke to be selected
-
 
 
 
@@ -23,13 +21,12 @@ class KnockKnockJokes:
     def whosThere(self, response, jokes):
         lc_response = response.lower()
 
-        for joke in jokes:
         # chooses a random joke
-            chosen_joke = random.choice(joke)
-            punchline = chosen_joke
+        chosen_joke = random.choice(jokes)
+        punchline = chosen_joke['punchline']
 
         if lc_response == str and (response == "whos there?" or "whos there"):
-            return punchline_pt1
+            return punchline
         else:
             return "Pssssttttt!!! You're supposed to say, 'Who's there?' >.<"
     
@@ -41,38 +38,38 @@ class KnockKnockJokes:
 
 # the ANimal Jokes
 animal_jokes = [
-    {'punchline1':'Moose', "final_punchline":'Moose you bein\' so nosy!'},
-    {'punchline2':'Poodle', "final_punchline":'Poodle a lil mustard on my hot dog please!'},
-    {'punchline3':'Iguana', "final_punchline":'Iguana hold your hand.'},
-    {'punchline4':'Goat', "final_punchline":'Goat to the door and find out!'},
-    {'punchline5':'Alpaca', "final_punchline":'Alpaca the trunk, you pack-a the suitcase!'}
+    {'punchline':'Moose', "final_punchline":'Moose you bein\' so nosy!'},
+    {'punchline':'Poodle', "final_punchline":'Poodle a lil mustard on my hot dog please!'},
+    {'punchline':'Iguana', "final_punchline":'Iguana hold your hand.'},
+    {'punchline':'Goat', "final_punchline":'Goat to the door and find out!'},
+    {'punchline':'Alpaca', "final_punchline":'Alpaca the trunk, you pack-a the suitcase!'}
     ]
 
 # The Food jokes
 food_jokes = [
-    {'punchline1':'Peas', "final_punchline":'Peas to meet you!'},
-    {'punchline2':'Potatoes', "final_punchline":'Potatoes don\'t have a last name silly!'},
-    {'punchline3':'Ice-cream Soda', "final_punchline":'ICE SCREAM SODA WHOLE WORLD CAN HEAR ME!'},
-    {'punchline4':'Muffin', "final_punchline":'Theres muffin the matter with me. I\'m doing fine!'},
-    {'punchline5':'Butter', "final_punchline":'Butter bring an umbrella - it looks like it\'s rainin\'!'}
+    {'punchline':'Peas', "final_punchline":'Peas to meet you!'},
+    {'punchline':'Potatoes', "final_punchline":'Potatoes don\'t have a last name silly!'},
+    {'punchline':'Ice-cream Soda', "final_punchline":'ICE SCREAM SODA WHOLE WORLD CAN HEAR ME!'},
+    {'punchline':'Muffin', "final_punchline":'Theres muffin the matter with me. I\'m doing fine!'},
+    {'punchline':'Butter', "final_punchline":'Butter bring an umbrella - it looks like it\'s rainin\'!'}
 ]
 
 # The spooky jokes
 spooky_jokes = [
-    {'punchline1':'Harry', "final_punchline":'Harry spider crawling up your back!'},
-    {'punchline2':'Ivana', "final_punchline":'Ivana suck your blood!'},
-    {'punchline3':'Ooze', "final_punchline":'Ooze that monster under yer bed!'},
-    {'punchline4':'Howie', "final_punchline":'Howie gonna hide this dead body?'},
-    {'punchline5':'Doughnut', "final_punchline":'Doughnut worry, I shall give yer a swift death!'}
+    {'punchline':'Harry', "final_punchline":'Harry spider crawling up your back!'},
+    {'punchline':'Ivana', "final_punchline":'Ivana suck your blood!'},
+    {'punchline':'Ooze', "final_punchline":'Ooze that monster under yer bed!'},
+    {'punchline':'Howie', "final_punchline":'Howie gonna hide this dead body?'},
+    {'punchline':'Doughnut', "final_punchline":'Doughnut worry, I shall give yer a swift death!'}
 ]
 
 # the random jokes
 out_of_this_world_jokes = [
-    {'punchline1':'Ash', "final_punchline":'Sounds like you have a cold!'},
-    {'punchline2':'Sadie', "final_punchline":'Sadie magic word and watch me disappear!'},
-    {'punchline3':'Cash', "final_punchline":'No thanks, I\'ll have some peanuts.'},
-    {'punchline4':'Luke', "final_punchline":'Luke through the peephole and find out!'},
-    {'punchline5':'Roach', "final_punchline":'Roach you an email last week and i\'m still waiting for a response.'}
+    {'punchline':'Ash', "final_punchline":'Sounds like you have a cold!'},
+    {'punchline':'Sadie', "final_punchline":'Sadie magic word and watch me disappear!'},
+    {'punchline':'Cash', "final_punchline":'No thanks, I\'ll have some peanuts.'},
+    {'punchline':'Luke', "final_punchline":'Luke through the peephole and find out!'},
+    {'punchline':'Roach', "final_punchline":'Roach you an email last week and i\'m still waiting for a response.'}
 ]
 
 # PLayer & Welcome
@@ -98,5 +95,5 @@ if category_lowercase == "animal":
     # the player responds
     player_response = input()
     # respond to the player's 'whos there'
-    animal_player.whosThere(player_response, punchline)
+    animal_player.whosThere(player_response, animal_jokes)
 
